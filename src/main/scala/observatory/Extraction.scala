96eaa41d571e7219a78e861f6c5805dd29c1516b
Gavri Fernandez <gavri.fernandez@gmail.com>
2018-07-18 18:44:22 +0530

    Extraction: locateTemperatures and locationYearlyAverageRecords

--- src/main/scala/observatory/Extraction.scala
+++ src/main/scala/observatory/Extraction.scala
@@ -2,10 +2,21 @@ package observatory
 
 import java.time.LocalDate
 
+import org.apache.spark.sql.{Dataset, SparkSession}
+
 /**
   * 1st milestone: data extraction
   */
 object Extraction {
+
+  case class StationRecord(stn: Int, wban: Option[Int], latitude: Double, longitude: Double) {
+    def location: Location = Location(latitude, longitude)
+  }
+
+  case class TemperatureRecord(stn: Int, wban: Option[Int], month: Int, day: Int, temperature: Temperature)
+
+  val spark = SparkSession.builder.appName("observatory").master("local").getOrCreate()
+  import spark.implicits._
 
   /**
     * @param year             Year number
@@ -14,7 +25,25 @@ object Extraction {
     * @return A sequence containing triplets (date, location, temperature)
     */
   def locateTemperatures(year: Year, stationsFile: String, temperaturesFile: String): Iterable[(LocalDate, Location, Temperature)] = {
-    ???
+    val stationsFilePath = this.getClass.getResource(stationsFile).toURI.toString
+    val stations: Dataset[StationRecord] = spark.read.option("inferSchema", true).csv(stationsFilePath).as[StationRecord]
+    val temperaturesFilePath = this.getClass.getResource(temperaturesFile).toURI.toString
+    val temperatures: Dataset[TemperatureRecord] = spark.read.option("inferSchema", true).csv(temperaturesFilePath).as[TemperatureRecord]
+    locateTemperaturesFromRecords(stations, temperatures).collect.map { case(month, day, location, temperature) =>
+      (LocalDate.of(year, month, day), location, temperature)
+    }
+  }
+
+
+  def locateTemperaturesFromRecords(stations: Dataset[StationRecord], temperatures: Dataset[TemperatureRecord]): Dataset[(Int, Int, Location, Temperature)] = {
+    val joined: Dataset[(StationRecord, TemperatureRecord)] = stations.joinWith(temperatures, stations("stn") === temperatures("stn"))
+    joined.map { (record: (StationRecord, TemperatureRecord)) =>
+      val stationRecord = record._1
+      val temperatureRecord = record._2
+      val location = stationRecord.location
+      val temperature = temperatureRecord.temperature
+      (temperatureRecord.month, temperatureRecord.day, location, temperature)
+    }
   }
 
   /**
@@ -22,7 +51,11 @@ object Extraction {
     * @return A sequence containing, for each location, the average temperature over the year.
     */
   def locationYearlyAverageRecords(records: Iterable[(LocalDate, Location, Temperature)]): Iterable[(Location, Temperature)] = {
-    ???
+    val resultAsMap = records.groupBy(_._2).mapValues { records =>
+      val temperatures = records.map(_._3)
+      temperatures.sum / temperatures.size
+    }
+    resultAsMap.toList
   }
 
 }
--- src/test/scala/observatory/ExtractionTest.scala
+++ src/test/scala/observatory/ExtractionTest.scala
@@ -1,10 +1,46 @@
 package observatory
 
-import org.junit.runner.RunWith
 import org.scalatest.FunSuite
-import org.scalatest.junit.JUnitRunner
+import java.time.LocalDate
+
+import observatory.Extraction.{StationRecord, TemperatureRecord}
+import org.apache.spark.sql.SparkSession
 
 trait ExtractionTest extends FunSuite {
 
-  
+  test("location yearly average records degenerate case") {
+    assert(Iterable.empty == Extraction.locationYearlyAverageRecords(Iterable.empty))
+  }
+
+  test("location average records") {
+    val records = List((LocalDate.of(2010, 1, 2), Location(1.0, 2.0), 5.0), (LocalDate.of(2010, 3, 4), Location(1.0, 2.0), 10.0),
+      (LocalDate.of(2010, 2, 3), Location(10.0, 15.0), 7.0), (LocalDate.of(2010, 5, 6), Location(10.0, 15.0), 9.0))
+    val actual = Extraction.locationYearlyAverageRecords(records)
+    val expected = List((Location(1.0, 2.0), 7.5), (Location(10.0, 15.0), 8.0))
+    assert(expected.toSet == actual.toSet)
+  }
+
+  test("locate temperature from records degenerate case") {
+    val spark = SparkSession.builder.appName("observatory").master("local").getOrCreate()
+    import spark.implicits._
+
+    val actual = Extraction.locateTemperaturesFromRecords(
+      (List() : List[StationRecord]).toDS,
+      (List() : List[TemperatureRecord]).toDS
+    )
+
+    assert(actual.collect.isEmpty)
+  }
+
+  test("locate temperature from records") {
+    val spark = SparkSession.builder.appName("observatory").master("local").getOrCreate()
+    import spark.implicits._
+
+    val actual = Extraction.locateTemperaturesFromRecords(
+      List(Extraction.StationRecord(1, Some(2), 5, 7)).toDS,
+      List(Extraction.TemperatureRecord(1, Some(2), 1, 2, 5.0)).toDS
+    )
+
+    assert(actual.collect.deep == Array((1, 2, Location(5.0, 7.0), 5.0)).deep)
+  }
 }
